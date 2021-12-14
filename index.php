bbc547768b162b90f0c0845aeb8cfbf9d04c8ba6
Alex Oderdenge <alexeksa@gmail.com>
2021-12-14 14:12:56 -0300

    Finished, there is only a bug to fix

--- index.php
+++ index.php
@@ -13,7 +13,16 @@
       <title>PHP Crud Test</title>
    </head>
    <body>
-      <?php require_once 'process.php';?>
+      <?php require_once 'process.php'; ?>
+      <?php if (isset($_SESSION['message'])): ?>
+      <div   class="alert alert-<?=$_SESSION['msg_type']?>">
+         <?php
+            
+            echo $_SESSION['message'];
+            unset($_SESSION['message']);
+         ?>
+      </div>
+      <?php endif ?>
       <div class="container">
          <?php
             $mysqli = new mysqli('localhost', 'root', '', 'crud') or die (mysqli_error($mysqli));
@@ -56,16 +65,21 @@
          ?>
          <div class="row justify-content-center">
             <form action="process.php" method="post">
+               <input type="hidden" name="id" value="<?php echo $id;?>">
                <div class="form-group">
                   <label>Name</label>
-                  <input type="text" name="name" class="form-control" value="Enter your name">
+                  <input type="text" name="name" class="form-control" value="<?php echo $name ?>" placeholder="Enter your name">
                </div>
                <div class="form-group">
                   <label>location</label>
-                  <input type="text" name="location" class="form-control" value="Enter your location">
+                  <input type="text" name="location" class="form-control" value="<?php echo $location ?>" placeholder="Enter your location">
                </div>
                <div class="form-group">
-                  <button type="submit" class="btn btn-primary" name="save">Save</button>
+                  <?php if ($update == true): ?>
+                     <button type="submit" class="btn btn-info" name="update">Update</button>
+                  <?php else: ?>
+                     <button type="submit" class="btn btn-primary" name="save">Save</button>
+                  <?php endif ?>
                </div>
             </form>
          </div>
--- process.php
+++ process.php
@@ -1,19 +1,58 @@
 <?php
+//Youtube video used to make this repo: https://www.youtube.com/watch?v=3xRMUDC74Cw
 
-
-$msqli = new mysqli('localhost', 'root', '', 'crud') or die (mysqli_error($mysqli));
+session_start();
+$mysqli = new mysqli('localhost', 'root', '', 'crud') or die (mysqli_error($mysqli));
+$id = 0;
+$update = false;
+$name = '';
+$location = '';
 
 
 if (isset($_POST['save'])) {
    $name = $_POST['name'];
    $location = $_POST['location'];
-   $msqli->query("INSERT INTO data (name, location) VALUES ('$name', '$location')") or die(mysqli_error($mysqli));
+   $mysqli->query("INSERT INTO data (name, location) VALUES ('$name', '$location')") or die(mysqli_error($mysqli));
+   
+  
+   $_SESSION['message'] = "Record has been saved!";
+   $_SESSION['msg_type'] = "success";
 
+   header("location: index.php");
 }
-// FIXME 
+
+// FIXME
 if (isset($_GET['delete'])) {
+   //This line below isn't been read inside this if condition
+   $_SESSION['message'] = "Record has been deleted!";
+   $_SESSION['msg_type'] = "danger";
    $id = $_GET['delete'];
-   $mysqli->query("DELETE FROM data WHERE id=$id");
+   $mysqli->query("DELETE FROM data WHERE id=$id") or die (mysqli_error($mysqli));
+   header("location: index.php");
+}
+
+if (isset($_GET['edit'])){
+   $id = $_GET['edit'];
+   $update = true;
+   $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die (mysqli_error($mysqli));
+   if (count(array($result)) == 1) {
+      $row = $result->fetch_array();
+      $name = $row['name'];
+      $location = $row['location'];
+   }
+}
+
+if (isset($_POST['update'])) {
+   $id = $_POST['id'];
+   $name = $_POST['name'];
+   $location = $_POST['location'];
+
+   $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id = $id") or die (mysqli_error($mysqli));
+
+   $_SESSION['message'] = "Record has been updated!";
+   $_SESSION['msg_type'] = "warning";
+   header("location: index.php");
+
 }
 
 ?>
